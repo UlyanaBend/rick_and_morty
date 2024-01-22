6ea7154ba1ea81182f9574f4541228f98c0b209f
Ulyana <ulanabend1@gmail.com>
2024-01-22 14:20:15 +0200

    add HttpLoggingInterceptor and OkHttpClient

--- app/src/main/java/com/example/rick_and_morty/MainActivity.kt
+++ app/src/main/java/com/example/rick_and_morty/MainActivity.kt
@@ -8,6 +8,8 @@ import com.example.rick_and_morty.model.CharacterAPI
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -16,10 +18,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
         val tvModel = findViewById<TextView>(R.id.tvModel)
 
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://rickandmortyapi.com/api/")
+            .baseUrl("https://rickandmortyapi.com/api/").client(client)
             .addConverterFactory(GsonConverterFactory.create()).build()
         val characterAPI = retrofit.create(CharacterAPI::class.java)
 

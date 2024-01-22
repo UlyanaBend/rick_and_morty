1a9052faf32278a343c6f517c148ac92b59fedeb
Ulyana <ulanabend1@gmail.com>
2024-01-22 20:59:25 +0200

    add recyceleview to the fragment_characters_list

--- app/src/main/java/com/example/rick_and_morty/model/CharacterAPI.kt
+++ app/src/main/java/com/example/rick_and_morty/model/CharacterAPI.kt
@@ -6,4 +6,7 @@ import retrofit2.http.Path
 interface CharacterAPI {
     @GET("character/{id}")
     suspend fun getCharacterById(@Path("id") id: Int): Character
+
+    @GET("character/1,2,3,4,5,6,7,8,9,10,11,12,13,14")
+    suspend fun getAllProducts() : List<Character>
 }

9d3200bcb0b67c380a34a73daedfaf802e429d97
Ulyana <60302243+UlyanaBend@users.noreply.github.com>
2024-01-26 14:37:03 +0200

    Retrofit feature (#2)
    * add getCharacterById in the MainActivity
    * add HttpLoggingInterceptor and OkHttpClient
    * add ImageView

--- app/src/main/java/com/example/rick_and_morty/MainActivity.kt
+++ app/src/main/java/com/example/rick_and_morty/MainActivity.kt
@@ -2,6 +2,18 @@ package com.example.rick_and_morty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.example.rick_and_morty.model.Character
+import com.example.rick_and_morty.model.CharacterAPI
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
